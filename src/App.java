1bdea4def1ae0e1f06b530ed6a8edcd078f4ba5d
Sicheng Yang <43109893+SichengYang@users.noreply.github.com>
2021-11-18 16:49:19 -0600

    Add files via upload

---
+++ src/App.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+import jpeg.JpegExif;
+import jpeg.Entry;
+
+public class App
+{
+	public static void main(String[] args) throws FileNotFoundException, IOException
+	{
+		Scanner input = new Scanner (System.in);
+		System.out.print("File name:");
+		String filename = input.next();
+		File photo = new File(filename);
+		BufferedInputStream f = new BufferedInputStream(new FileInputStream (photo));
+		try 
+		{
+			JpegExif exif = new JpegExif(f);
+			
+			Entry[] ifd0 = exif.getIfd0();
+			System.out.println("IFD0:");
+			for(Entry e : ifd0)
+				System.out.println(e);
+			ifd0 = null;
+			
+			Entry[] sub_ifd = exif.getSubIfd();
+			System.out.println("sub IFD:");
+			for(Entry e : sub_ifd)
+				System.out.println(e);
+			sub_ifd = null;
+			
+			Entry[] ifd1 = exif.getIfd1();
+			System.out.println("IFD1:");
+			for(Entry e : ifd1)
+				System.out.println(e);
+			ifd1 = null;
+			
+			Entry[] gps_ifd = exif.getGpsIfd();
+			System.out.println("GPS data:");
+			for(Entry e : gps_ifd)
+				System.out.println(e);
+			gps_ifd = null;
+		}
+		catch(Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+		f.close();
+		input.close();
+	}
+}
