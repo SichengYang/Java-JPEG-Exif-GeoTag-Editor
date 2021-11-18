e21eb6b7f5e57d913f54672e13d83f6c75fd1d9c
Sicheng Yang <43109893+SichengYang@users.noreply.github.com>
2021-11-18 16:39:24 -0600

    Add files via upload

---
+++ org/commons/tests/BigEndianTest.java
@@ -0,0 +1,19 @@
+package org.commons.tests;
+
+import org.commons.endian.BigEndian;
+
+public class BigEndianTest {
+
+	public static void main(String[] args) {
+		byte data1 = (byte)(0xF0);
+		byte data2 = (byte)(0x0F);
+		byte data3 = (byte)(0xA0);
+		byte data4 = (byte)(0x0A);
+		
+		System.out.println(BigEndian.getLong32(data1, data2, data3, data4));
+		System.out.println(BigEndian.getInt16(data1, data2));
+		System.out.println(BigEndian.getInt32(data1, data2, data3, data4));
+		System.out.println(BigEndian.getSignedShort(data1, data2));
+	}
+
+}
