66642498d66061fc25b182f7c6066af168f1c1d2
杨思成 <43109893+SichengYang@users.noreply.github.com>
2021-12-13 02:52:31 -0600

    update

--- src/Editor.java
+++ src/Editor.java
@@ -7,7 +7,6 @@ import jpeg.Jpeg;
 import jpeg.JpegOutputSet;
 import jpeg.NotJpegException;
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -15,9 +14,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.Dragboard;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -35,6 +31,7 @@ public class Editor extends Application{
     private static FileChooser fileChoose = new FileChooser();
 
     private static final String CANCEL_TEXT = "cancel";
+    
     public static void main(String[] args)
     {
         launch();
